e11d20dc3a449a9ced343c967372ea52e589faa7
Nguyen Trong Quoc Huy <nguyentrongquochuy.eni@gmail.com>
2024-05-01 06:33:38 +0700

    240501

--- 2_LinearRegression/SGD_based_Optimization_by_linear_regression.py
+++ 2_LinearRegression/SGD_based_Optimization_by_linear_regression.py
@@ -1,21 +1,16 @@
 #SGD Stochatis Gradient Descent
 #Step1: Shuffle the data
 #Step2: Select training sample and testing sample. Base on ratio: len(list_training_sample)) / len(list_data)
+
 import numpy as np
 import random
-import Optimization_by_linear_regression
+import Optimization_by_linear_regression_based_gradient_derivation
 import matplotlib.pyplot as plt
 dataset_folder      =   r'C:\Github_NTQH\AIO2023_Main_Lesson_Code\2_LinearRegression\dataset' 
 dataset_file_name   =   r'\t32_1.csv'
 dataset_file_path   =   dataset_folder  + dataset_file_name
 lst_data            =   np.genfromtxt(dataset_file_path,delimiter=',',skip_header=1).tolist()
 
-random.shuffle(lst_data)
-
-print('pls input ratio between training and testing')
-ratio                   =   float(input())
-training_data           =   lst_data[0:int(ratio*len(lst_data))]
-testing_data            =   lst_data[int(ratio*len(lst_data)):len(lst_data)]
 
 #################################################################################################################################################################################  ***   
 #-------------------------------------------Initialize_pre-train_parameters-----------------------------                                                                        #  *** 
@@ -27,7 +22,7 @@ training_data           =   lst_data[0:int(ratio*len(lst_data))]                
 testing_data            =   lst_data[int(ratio*len(lst_data)):len(lst_data)]                                                                                                    #   III 
 #=======================================================================================================                                                                        #   NNN   
 w_b_quantity                =   len(training_data[0])                                                                                                                           #   III
-ini_lst_w_b                 =   Optimization_by_linear_regression.initialize_parameter_weight_bias(w_b_quantity)    # then we have initialize weight and bias                   #   TTT
+ini_lst_w_b                 =   Optimization_by_linear_regression_based_gradient_derivation.initialize_parameter_weight_bias(w_b_quantity)    # ini weight and bias             #   TTT
 print(f'List ini weights and bias ={ini_lst_w_b}')                                                                                                                              #   III
 #=======================================================================================================                                                                        #   AAA
 print(f'\nPls input the quantity of epoch for trainning this SGD model')                                                                                                        #   LLL
@@ -58,12 +53,12 @@ print('\nIf you want to train by the way to update one-by-one for every sample: 
 method_define           =   str(input())
 if method_define        ==   "obo":
     print('Well! you chose "obo"')
-    model_lst_w_b,model_loss,lst_loss_val           =   Optimization_by_linear_regression.training_all_database_obo(epoch, lr, ini_lst_w_b, lst_data_training_feature, lst_data_training_label)
+    model_lst_w_b,model_loss,lst_loss_val           =   Optimization_by_linear_regression_based_gradient_derivation.training_all_database_obo(epoch, lr, ini_lst_w_b, lst_data_training_feature, lst_data_training_label)
     print(f'\nAfter train by method "obo",we get: \nmodel parameter {model_lst_w_b}\nAnd the loss of model is {model_loss}')
     
 elif method_define      ==  "avg":
     print('Well! you chose "avg"')
-    model_lst_w_b,model_loss,lst_loss_val           =   Optimization_by_linear_regression.training_all_database_average(epoch, lr, ini_lst_w_b, lst_data_training_feature, lst_data_training_label)
+    model_lst_w_b,model_loss,lst_loss_val           =   Optimization_by_linear_regression_based_gradient_derivation.training_all_database_average(epoch, lr, ini_lst_w_b, lst_data_training_feature, lst_data_training_label)
     print(f'\nAfter train by method "avg", we get: \nmodel parameter {model_lst_w_b}\nAnd the loss of model is {model_loss}')
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -71,7 +66,7 @@ elif method_define      ==  "avg":
 
 #-----------------------------------------------------------------------SGD_model_testing_process--------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------&_Visualization----------------------------------------------------------------------------------
-lst_predict_value                   =   Optimization_by_linear_regression.compute_predict_value(model_lst_w_b, lst_data_testing_feature)
+lst_predict_value                   =   Optimization_by_linear_regression_based_gradient_derivation.compute_predict_value(model_lst_w_b, lst_data_testing_feature)
 
 
 #https://matplotlib.org/stable/gallery/text_labels_and_annotations/align_ylabels.html#sphx-glr-gallery-text-labels-and-annotations-align-ylabels-py
